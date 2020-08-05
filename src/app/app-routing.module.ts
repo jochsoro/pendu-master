a99f9a2c423d8b1ec1d8785180858cd0b3401e2a
joch soro <joch.soro@gmail.com>
2020-08-05 11:09:37 +0200

    Ajout de la page des stats du jeux

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'stats',
+    loadChildren: () => import('./pages/stats/stats.module').then( m => m.StatsPageModule)
+  },
 ];
 
 @NgModule({
